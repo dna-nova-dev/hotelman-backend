bc770dd815742309e2d7a7b4d134398275b562d1
Alexis Leon Reyes <83489240+ToshioDev@users.noreply.github.com>
2024-09-11 00:37:53 -0600

    fix: analytics

--- handlers/analytics_handler.go
+++ handlers/analytics_handler.go
@@ -19,16 +19,14 @@ type AnalyticsHandler struct {
 
 // AnalyticsResponse define la estructura de la respuesta de análisis
 type AnalyticsResponse struct {
-	TotalPriceGuest float64 json:"totalPriceGuest"
+	TotalPriceGuest float64 `json:"totalPriceGuest"`
 	Guest           struct {
-		Total    int json:"total"
-		Occupied int json:"occupied"
-	} json:"guest"
+		Total int `json:"total"`
+	} `json:"guest"`
 	Rental struct {
-		Total    int json:"total"
-		Occupied int json:"occupied"
-	} json:"rental"
-	TotalClients int json:"totalClients"
+		Total int `json:"total"`
+	} `json:"rental"`
+	TotalClients int `json:"totalClients"`
 }
 
 // GetAnalyticsHandler maneja la obtención de datos de análisis
@@ -79,7 +77,7 @@ func (h *AnalyticsHandler) GetAnalyticsHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	var priceSumResult struct {
-		TotalPrice float64 bson:"totalPrice"
+		TotalPrice float64 `bson:"totalPrice"`
 	}
 
 	if cursor.Next(context.Background()) {
@@ -102,35 +100,12 @@ func (h *AnalyticsHandler) GetAnalyticsHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	var guestTotalCountResult struct {
-		Total int bson:"total"
+		Total int `bson:"total"`
 	}
 
 	if cursor.Next(context.Background()) {
 		if err := cursor.Decode(&guestTotalCountResult); err != nil {
 			http.Error(w, "Failed to decode total guest rooms", http.StatusInternalServerError)
-			return
-		}
-	}
-
-	// Obtener el total de habitaciones de huéspedes ocupadas
-	guestOccupiedPipeline := mongo.Pipeline{
-		{{"$match", bson.D{{"roomType", "guest"}, {"status", "occupied"}}}},
-		{{"$count", "occupied"}},
-	}
-
-	cursor, err = h.Client.Database(constants.MongoDBDatabase).Collection("rooms").Aggregate(context.Background(), guestOccupiedPipeline)
-	if err != nil {
-		http.Error(w, "Failed to calculate occupied guest rooms", http.StatusInternalServerError)
-		return
-	}
-
-	var guestOccupiedCountResult struct {
-		Occupied int bson:"occupied"
-	}
-
-	if cursor.Next(context.Background()) {
-		if err := cursor.Decode(&guestOccupiedCountResult); err != nil {
-			http.Error(w, "Failed to decode occupied guest rooms", http.StatusInternalServerError)
 			return
 		}
 	}
@@ -148,35 +123,12 @@ func (h *AnalyticsHandler) GetAnalyticsHandler(w http.ResponseWriter, r *http.Re
 	}
 
 	var rentalTotalCountResult struct {
-		Total int bson:"total"
+		Total int `bson:"total"`
 	}
 
 	if cursor.Next(context.Background()) {
 		if err := cursor.Decode(&rentalTotalCountResult); err != nil {
 			http.Error(w, "Failed to decode total rental rooms", http.StatusInternalServerError)
-			return
-		}
-	}
-
-	// Obtener el total de habitaciones de renta ocupadas
-	rentalOccupiedPipeline := mongo.Pipeline{
-		{{"$match", bson.D{{"roomType", "rental"}, {"status", "occupied"}}}},
-		{{"$count", "occupied"}},
-	}
-
-	cursor, err = h.Client.Database(constants.MongoDBDatabase).Collection("rooms").Aggregate(context.Background(), rentalOccupiedPipeline)
-	if err != nil {
-		http.Error(w, "Failed to calculate occupied rental rooms", http.StatusInternalServerError)
-		return
-	}
-
-	var rentalOccupiedCountResult struct {
-		Occupied int bson:"occupied"
-	}
-
-	if cursor.Next(context.Background()) {
-		if err := cursor.Decode(&rentalOccupiedCountResult); err != nil {
-			http.Error(w, "Failed to decode occupied rental rooms", http.StatusInternalServerError)
 			return
 		}
 	}
@@ -192,18 +144,14 @@ func (h *AnalyticsHandler) GetAnalyticsHandler(w http.ResponseWriter, r *http.Re
 	response := AnalyticsResponse{
 		TotalPriceGuest: priceSumResult.TotalPrice,
 		Guest: struct {
-			Total    int json:"total"
-			Occupied int json:"occupied"
+			Total int `json:"total"`
 		}{
-			Total:    guestTotalCountResult.Total,
-			Occupied: guestOccupiedCountResult.Occupied,
+			Total: guestTotalCountResult.Total,
 		},
 		Rental: struct {
-			Total    int json:"total"
-			Occupied int json:"occupied"
+			Total int `json:"total"`
 		}{
-			Total:    rentalTotalCountResult.Total,
-			Occupied: rentalOccupiedCountResult.Occupied,
+			Total: rentalTotalCountResult.Total,
 		},
 		TotalClients: int(clientTotalCount),
 	}
